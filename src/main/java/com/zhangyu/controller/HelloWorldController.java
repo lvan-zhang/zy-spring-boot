8c4beed1d1e532a0b7cc85eda9da04686bb36293
zhangyu <zhangy@cmvalue.com>
2023-11-21 18:48:30 +0800

    feat: 添加环境变量

--- src/main/java/com/zhangyu/controller/HelloWorldController.java
+++ src/main/java/com/zhangyu/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.zhangyu.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+//    TODO:
+    @Value("${info.app.name}")
+    private String appName;
+
     @RequestMapping("/hello")
     public String index(){
-        return "Hello World5";
+        return this.appName;
     }
 }
