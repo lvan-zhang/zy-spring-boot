4d2b6a613486c98806c13200a669d4b2ac29e116
zhangyu <zhangy@cmvalue.com>
2023-11-28 17:23:00 +0800

    feat: jpa

--- src/main/java/com/zhangyu/ZySpringBootApplication.java
+++ src/main/java/com/zhangyu/ZySpringBootApplication.java
@@ -2,9 +2,9 @@ package com.zhangyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//开启定时任务
 //@EnableScheduling
 public class ZySpringBootApplication {
 
