57ee3898d6054f55ad10b24c075e68fcba1bf5d3
zhangyu <865091936@qq.com>
2024-09-26 16:19:53 +0800

    feat: 主项目模块

---
+++ zy-rabbitmq/src/main/java/com/zhangyu/ZyRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyRabbitmqApplication.class, args);
+    }
+
+}
---
+++ zy-rabbitmq/src/test/java/com/zhangyu/rabbit/HelloTest.java
@@ -0,0 +1,20 @@
+package com.zhangyu.rabbit;
+
+import com.zhangyu.rabbit.hello.HelloSender;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloTest {
+
+	@Autowired
+	private HelloSender helloSender;
+
+	@Test
+	public void hello() throws Exception {
+		helloSender.send();
+	}
+
+
+}
